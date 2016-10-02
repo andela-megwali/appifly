41b317e2517045285b5ecb555688420ce4649a32
andela-megwali <michael.egwali@andela.com>
2016-10-02 02:14:24 +0100

    Split user controller login function into session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,38 @@
 class SessionsController < ApplicationController
+  def login
+  end
+
+  def attempt_login
+    check_user
+    authorize_the_user
+  end
+
+  def logout
+    session[:user_id] = nil
+    session[:user_username] = nil
+    redirect_to login_path, notice: "User successfully signed out."
+  end
+
+  private
+
+  def user_params
+    params.require(:sign_in).permit(:username, :password)
+  end
+
+  def check_user
+    if params[:sign_in][:username] && params[:sign_in][:password]
+      User.find_by(username: params[:sign_in][:username])
+    end
+  end
+
+  def authorize_the_user
+    if check_user
+      authorized_user = check_user.authenticate(params[:sign_in][:password])
+      session[:user_id] = authorized_user.id
+      session[:user_username] = authorized_user.username
+      redirect_to past_bookings_path, notice: "User successfully signed in."
+    else
+      redirect_to login_path, notice: "Invalid password or username"
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,20 +44,6 @@ class UsersController < ApplicationController
     redirect_to user_url, notice: "User was successfully destroyed"
   end
 
-  def login
-  end
-
-  def attempt_login
-    check_user
-    authorize_the_user
-  end
-
-  def logout
-    session[:user_id] = nil
-    session[:user_username] = nil
-    redirect_to login_path, notice: "User successfully signed out."
-  end
-
   private
 
   def set_user
@@ -68,23 +54,6 @@ class UsersController < ApplicationController
     params.require(:user).permit :title, :firstname, :lastname, :username,
                                  :password, :email, :telephone,
                                  :subscription
-  end
-
-  def check_user
-    if params[:sign_in][:username] && params[:sign_in][:password]
-      User.find_by(username: params[:sign_in][:username])
-    end
-  end
-
-  def authorize_the_user
-    if check_user
-      authorized_user = check_user.authenticate(params[:sign_in][:password])
-      session[:user_id] = authorized_user.id
-      session[:user_username] = authorized_user.username
-      redirect_to past_bookings_path, notice: "User successfully signed in."
-    else
-      redirect_to login_path, notice: "Invalid password or username"
-    end
   end
 
   def send_welcome_email
