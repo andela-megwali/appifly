f414eb9a8ad97c651fffd2218ce3390d56a65905
andela-megwali <michael.egwali@andela.com>
2016-10-13 12:24:25 +0100

    helper spec

--- spec/helpers/welcome_helper_spec.rb
+++ spec/helpers/welcome_helper_spec.rb
@@ -1,23 +1,57 @@
 require "rails_helper"
 
-# Specs in this file have access to a helper object that includes
-# the ManageBookingHelper. For example:
-#
-# describe ManageBookingHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe WelcomeHelper, type: :helper do
-  # describe "#multiplier" do
-  #   it "assigns a value to a travel class" do
-  #     expect(multiplier["Economy"]).to eq 1
-  #     expect(multiplier["Business"]).to eq 1.5
-  #     expect(multiplier["First"]).to eq 2
+  # params = {
+  #   enquiry: {
+  #     origin: "Lagos (LOS)",
+  #     destination: "Abuja (ABV)",
+  #     departure: Time.now,
+  #     view_format: "Grid",
+  #     travel_class: "Business",
+  #     passenger: "2",
+  #     subscription: true,
+  #     admin: true,
+  #     sql: "Yes",
+  #   }
+  # }
+  # before do
+  #   3.times do
+  #     create :flight, airline: Faker::Company.name
+  #     create :flight, :jfk_flight, airline: Faker::Company.name
+  #     create :flight, :cancelled, airline: Faker::Company.name
   #   end
+  #   get root_path, enquiry: params[:enquiry]
   # end
 
+  describe "#multiplier" do
+    it "assigns a value to a travel class" do
+      expect(multiplier["Economy"]).to eq 1
+      expect(multiplier["Business"]).to eq 1.5
+      expect(multiplier["First"]).to eq 2
+    end
+  end
+
+  describe "#passenger_class" do
+    context "when passenger class is valid" do
+      before { @passenger_enquiry = { class_selected: "Business" } }
+      it "assigns the value to passenger_class" do
+        expect(passenger_class).to eq "Business"
+      end
+    end
+
+    context "when passenger class is not valid" do
+      before { @passenger_enquiry = { class_selected: "Forever" } }
+      it "assigns default value to passenger_class" do
+        expect(passenger_class).to eq "Economy"
+      end
+    end
+  end
   
+  describe "#travel_value" do
+    before { @passenger_enquiry = { class_selected: "Business" } }
+    it "assigns a numerical value to travel value" do
+      expect(travel_value).to eq 1.5
+    end
+  end
+
 end
